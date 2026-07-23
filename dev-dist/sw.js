1470a03a1d9c7ac500d42527c038095906da4b27
Timur <utility.payments@example.com>
2026-07-23 20:52:55 +0300

    fix: fix invalid css keyframes syntax breaking the CI build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.b0mu5gg07j"
+    "revision": "0.941dkv59igk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
