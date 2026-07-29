63bb51ddc939070b95e9d3b1ecc5fa26715382bf
Timur <utility.payments@example.com>
2026-07-29 14:11:40 +0300

    Fix code block background inside markdown answers for light theme

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.3ts8ajggk4g"
+    "revision": "0.onle55o2eh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
