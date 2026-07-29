dba0a78ad95f15ed6451b8dd73c3a322a4e4d8ce
Timur <utility.payments@example.com>
2026-07-29 13:58:48 +0300

    Fix select dropdown dark background and label styling

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hkgdk5oeiq4"
+    "revision": "0.3ts8ajggk4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
